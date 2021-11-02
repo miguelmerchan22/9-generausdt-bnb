9ba0de170cf2cbb0a6fb257df7dde0b608de1ec9
miguelmerchan22 <miguelmerchan@hotmail.fr>
2021-11-02 17:58:58 -0600

    top

---
+++ src/cons.js
@@ -0,0 +1,15 @@
+const proxy = "https://proxy-sites.herokuapp.com/";
+
+const PRE = "https://precio-site.herokuapp.com/api/v1/servicio/precio/v2/SITE";
+
+const WS = "0xAFD76502Fb6EB876301f63B95F03EBcFc3c422fc";//0x0000000000000000000000000000000000000000 recibe los huerfanos por defecto
+
+const SCtest = "0xEC836b3F0fa7BF1579ab32C57A4e7dee971ccDcE";// direccion del contrato de pruebas test only no real
+
+const SC = "0x7C989de477ae67D542ced9296329c9717Bb42611";// direccion del contrato V1
+
+const SC2 = "0x7C989de477ae67D542ced9296329c9717Bb42611";// direccion del contrato V2
+
+const TOKEN = "0x55d398326f99059fF775485246999027B3197955";
+
+export default {proxy, WS, SCtest, SC, SC2, PRE, TOKEN};
